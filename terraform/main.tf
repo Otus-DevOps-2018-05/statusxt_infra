16e994d042acf7b95e5bd7a26da0efa1e2a5c48a
Daniil Tsebro <daniiltsebro@gmail.com>
2018-06-29 17:09:22 +1000

    Add lb.tf, readme modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,16 @@ provider "google" {
   project = "${var.project}"
   region  = "${var.region}"
 }
-
+resource "google_compute_project_metadata_item" "default" {
+  key = "ssh-keys"
+  value = "appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+}
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
+  name         = "reddit-app-${count.index}"
   machine_type = "g1-small"
   zone         = "${var.compute_zone}"
   tags         = ["reddit-app"]
+  count = "${var.count}"
 
   # определение загрузочного диска
   boot_disk {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,3 +23,8 @@ variable "compute_zone" {
   description = "Zone"
   default     = "europe-west1-b"
 }
+
+variable "count" {
+  description = "Count"
+  default     = "1"
+}
