72a34a2b02f08bd6198c84d5620754034f383c14
Daniil Tsebro <daniiltsebro@gmail.com>
2018-07-05 12:47:03 +1000

    Add modules, remote backend, stage and prod

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,27 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+}
+
+variable private_key_path {
+  description = "Path to the public key used to connect to instance"
+  default     = "~/.ssh/appuser"
+}
+
+variable zone {
+  description = "Zone"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable app_provisioner_toggle {
+  description = "Turn on/turn off provisioners"
+  default     = "1"
+}
+
+variable db_address {
+  description = "Turn on/turn off provisioners"
+  default     = ""
+}
---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,52 @@
+resource "google_compute_instance" "db" {
+  name         = "reddit-db"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-db"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.db_disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = "false"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    # правка конфига mongod - порт будет прослушиваться на внутреннем интерфейсе
+    inline = [
+      "sudo sed -i 's/^  bindIp: .*/  bindIp: ${google_compute_instance.db.network_interface.0.address}/g' /etc/mongod.conf",
+      "sudo systemctl restart mongod",
+    ]
+  }
+}
+
+resource "google_compute_firewall" "firewall_mongo" {
+  name    = "allow-mongo-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["27017"]
+  }
+
+  # правило применимо к инстансам с тегом ...
+  target_tags = ["reddit-db"]
+
+  # порт будет доступен только для инстансов с тегом ...
+  source_tags = ["reddit-app"]
+}
